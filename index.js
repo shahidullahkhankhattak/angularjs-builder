ef3a0bc77e33ec3667d179d0e04cd446a7417fa6
Shahid Ullah Khan <shahid.khan@hashehouse.com>
2019-03-01 17:20:02 +0500

    version 1.1.2 bug fixing in watching hasheconfig

--- index.js
+++ index.js
@@ -1,31 +1,106 @@
+/* libraries imports  */
 let fs = require('fs');
-var uglify = require("uglify-es");
+let uglify = require("uglify-es");
 const jsdom = require("jsdom");
 const rimraf = require("rimraf");
 const fsExtra = require("fs-extra");
 const {
     JSDOM
 } = jsdom;
-var distScripts = require('../../hasheconfig.json').scripts;
+let distScripts = JSON.parse(fs.readFileSync("hasheconfig.json").toString('utf8')).scripts;
+/* libraries imports ends */
 
 //get the index files
-var index = fs.readFileSync('index.html').toString('utf8');
+let index = fs.readFileSync('index.html').toString('utf8');
+
+//add index file html to global document variable
 global.document = new JSDOM(index).window.document;
 
-var args = process.argv;
+//get  (build, serve, and other argument from console command)
+let args = process.argv;
 
+// if no valid arguments are provided it will print help 
 if (args.indexOf('serve') == -1 && args.indexOf('build') == -1 && args.indexOf('dist') == -1) {
     console.log("\x1b[31m", "No arguments provided");
     console.log("\x1b[0m", "Use:");
     console.log("\x1b[0m", "\t\"hashe serve\" for watching files");
     console.log("\x1b[0m", "\t\"hashe dist\" for creating a build");
 } else if (args.indexOf('serve') > -1) {
+    // if files are being served
     console.log("\x1b[32m", "Watching for files changes ...");
 
-    var flipx = false;
+    //flipx variable is used to prevent dual event firing after change in file
+    let flipx = false;
+
+    // watch for changes in hashe config file
     fs.watch('hasheconfig.json', function () {
         if (flipx) {
-            var totalJs = {};
+            // if file is changed get old scripts into an oldscript variable
+            const oldScripts = distScripts;
+
+            // get new scripts (as hasheconfig file is changed)
+            distScripts = JSON.parse(fs.readFileSync("hasheconfig.json").toString('utf8')).scripts;
+            
+            // gets all new files that are added lastly
+            let newfiles = distScripts.filter(f => (oldScripts.indexOf(f) < 0));
+            
+            // loop through newfiles and watch for changes in those too.
+            newfiles.forEach(file => {
+                let flipNew = false;
+                fs.watch(file, function () {
+                    if (flipNew) {
+
+                        // hash table for all javascript files and their code
+                        let totalJs = {};
+
+                        // get all scripts and their code and add to hash table also remove those files form index.html page
+                        for (let j in distScripts) {
+                            console.log("\x1b[0m", "Compressing " + distScripts[j]);
+                            totalJs[distScripts[j]] = fs.readFileSync(distScripts[j]).toString('utf8');
+                            let scrpt = document.querySelector("script[src='"+distScripts[j]+"']");
+                            if(scrpt)
+                            document.body.removeChild(scrpt);
+                        }
+                        console.log("\x1b[0m", "Making angularjs build... ");
+
+                        // read main directory and delete the previous main-dist.js file
+                        fs.readdir('.', (err, files) => {
+                            for (let i = 0, len = files.length; i < len; i++) {
+                                let match = files[i].match(/main-dist.*.js/);
+                                if (match !== null)
+                                    fs.unlink(match[0], function () {});
+                            }
+                        });
+
+                        // all code is compressed and minified into this variable now
+                        let minified = uglify.minify(totalJs, {
+                            mangle: false
+                        });
+                        if (!minified.error) {
+                            // in case something wrong happens rollback the changes
+                            let uniqueName = Date.now() + Math.random(10000000000, 99999999999);
+                            fs.writeFileSync("main-dist." + uniqueName + ".js", minified.code);
+                            console.log("\x1b[32m", "Successfully build " + "main-dist." + uniqueName + ".js");
+        
+        
+                            let script = document.querySelector("script[src^='main-dist'");
+                            if (script)
+                                document.body.removeChild(script);
+                            let scriptNew = document.createElement("script");
+                            scriptNew.src = "main-dist." + uniqueName + ".js";
+                            document.body.appendChild(scriptNew);
+                            fs.writeFileSync('index.html', "<!doctype html>\n" + document.querySelector("html").outerHTML);
+                            console.log("\x1b[32m", "Watching for further files changes ...");
+                        } else {
+                            console.log("\x1b[31m", minified.error);
+                            console.log("\x1b[32m", "Watching for further files changes ...");
+                        }
+                    }
+                    flipNew = !flipNew;
+                });
+            });
+            
+            let totalJs = {};
             for (let j in distScripts) {
                 console.log("\x1b[0m", "Compressing " + distScripts[j]);
                 totalJs[distScripts[j]] = fs.readFileSync(distScripts[j]).toString('utf8');
@@ -35,8 +110,8 @@ if (args.indexOf('serve') == -1 && args.indexOf('build') == -1 && args.indexOf('
             }
             console.log("\x1b[0m", "Making angularjs build... ");
             fs.readdir('.', (err, files) => {
-                for (var i = 0, len = files.length; i < len; i++) {
-                    var match = files[i].match(/main-dist.*.js/);
+                for (let i = 0, len = files.length; i < len; i++) {
+                    let match = files[i].match(/main-dist.*.js/);
                     if (match !== null)
                         fs.unlink(match[0], function () {});
                 }
@@ -45,7 +120,7 @@ if (args.indexOf('serve') == -1 && args.indexOf('build') == -1 && args.indexOf('
                 mangle: false
             });
             if (!minified.error) {
-                var uniqueName = Date.now() + Math.random(10000000000, 99999999999);
+                let uniqueName = Date.now() + Math.random(10000000000, 99999999999);
                 fs.writeFileSync("main-dist." + uniqueName + ".js", minified.code);
                 console.log("\x1b[32m", "Successfully build " + "main-dist." + uniqueName + ".js");
 
@@ -66,11 +141,11 @@ if (args.indexOf('serve') == -1 && args.indexOf('build') == -1 && args.indexOf('
         flipx = !flipx;
     });
     //getting all scripts from index files
-    var flip = false;
+    let flip = false;
     for (let i in distScripts) {
         fs.watch("./" + distScripts[i], function () {
             if (flip) {
-                var totalJs = {};
+                let totalJs = {};
                 for (let j in distScripts) {
                     console.log("\x1b[0m", "Compressing " + distScripts[j]);
                     totalJs[distScripts[j]] = fs.readFileSync(distScripts[j]).toString('utf8');
@@ -80,8 +155,8 @@ if (args.indexOf('serve') == -1 && args.indexOf('build') == -1 && args.indexOf('
                 }
                 console.log("\x1b[0m", "Making angularjs build... ");
                 fs.readdir('.', (err, files) => {
-                    for (var i = 0, len = files.length; i < len; i++) {
-                        var match = files[i].match(/main-dist.*.js/);
+                    for (let i = 0, len = files.length; i < len; i++) {
+                        let match = files[i].match(/main-dist.*.js/);
                         if (match !== null)
                             fs.unlink(match[0], function () {});
                     }
@@ -90,7 +165,7 @@ if (args.indexOf('serve') == -1 && args.indexOf('build') == -1 && args.indexOf('
                     mangle: false
                 });
                 if (!minified.error) {
-                    var uniqueName = Date.now() + Math.random(10000000000, 99999999999);
+                    let uniqueName = Date.now() + Math.random(10000000000, 99999999999);
                     fs.writeFileSync("main-dist." + uniqueName + ".js", minified.code);
                     console.log("\x1b[32m", "Successfully build " + "main-dist." + uniqueName + ".js");
 
@@ -113,7 +188,7 @@ if (args.indexOf('serve') == -1 && args.indexOf('build') == -1 && args.indexOf('
     }
 } else if (args.indexOf('dist') > -1) {
     console.log("\x1b[32m", "Creating build ...");
-    var totalJs = {};
+    let totalJs = {};
     for (let j in distScripts) {
         console.log("\x1b[0m", "Compressing " + distScripts[j]);
         totalJs[distScripts[j]] = fs.readFileSync(distScripts[j]).toString('utf8');
@@ -123,8 +198,8 @@ if (args.indexOf('serve') == -1 && args.indexOf('build') == -1 && args.indexOf('
     }
     console.log("\x1b[0m", "Making angularjs build... ");
     fs.readdir('.', (err, files) => {
-        for (var i = 0, len = files.length; i < len; i++) {
-            var match = files[i].match(/main-dist.*.js/);
+        for (let i = 0, len = files.length; i < len; i++) {
+            let match = files[i].match(/main-dist.*.js/);
             if (match !== null)
                 fs.unlink(match[0], function () {});
         }
@@ -133,7 +208,7 @@ if (args.indexOf('serve') == -1 && args.indexOf('build') == -1 && args.indexOf('
         mangle: false
     });
     if (!minified.error) {
-        var uniqueName = Date.now() + Math.random(10000000000, 99999999999);
+        let uniqueName = Date.now() + Math.random(10000000000, 99999999999);
         fs.writeFileSync("main-dist." + uniqueName + ".js", minified.code);
         console.log("\x1b[32m", "Successfully build " + "main-dist." + uniqueName + ".js");
 
@@ -172,7 +247,7 @@ if (args.indexOf('serve') == -1 && args.indexOf('build') == -1 && args.indexOf('
     }
 }else if (args.indexOf('build') > -1) {
     console.log("\x1b[32m", "Creating build ...");
-    var totalJs = {};
+    let totalJs = {};
     for (let j in distScripts) {
         console.log("\x1b[0m", "Compressing " + distScripts[j]);
         totalJs[distScripts[j]] = fs.readFileSync(distScripts[j]).toString('utf8');
@@ -182,8 +257,8 @@ if (args.indexOf('serve') == -1 && args.indexOf('build') == -1 && args.indexOf('
     }
     console.log("\x1b[0m", "Making angularjs build... ");
     fs.readdir('.', (err, files) => {
-        for (var i = 0, len = files.length; i < len; i++) {
-            var match = files[i].match(/main-dist.*.js/);
+        for (let i = 0, len = files.length; i < len; i++) {
+            let match = files[i].match(/main-dist.*.js/);
             if (match !== null)
                 fs.unlink(match[0], function () {});
         }
@@ -192,7 +267,7 @@ if (args.indexOf('serve') == -1 && args.indexOf('build') == -1 && args.indexOf('
         mangle: false
     });
     if (!minified.error) {
-        var uniqueName = Date.now() + Math.random(10000000000, 99999999999);
+        let uniqueName = Date.now() + Math.random(10000000000, 99999999999);
         fs.writeFileSync("main-dist." + uniqueName + ".js", minified.code);
         console.log("\x1b[32m", "Successfully build " + "main-dist." + uniqueName + ".js");
 
